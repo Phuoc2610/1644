262ed9165f875c6a0f850b1c0091d6f4a0b65ef0
Phuoc <phuocnvgcd191236@fpt.edu.vn>
2022-10-13 09:31:21 +0700

    users

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express11111' });
+  res.render('index', { title: 'Express ATN Shop' });
+});
+router.get('/', function(req, res, next) {
+  res.render('login', { title: 'ATN Shop' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('users', { title: 'Welcome to user' });
 });
 
 module.exports = router;
